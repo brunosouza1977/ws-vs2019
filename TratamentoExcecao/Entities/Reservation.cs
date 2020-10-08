92aab4106149459ff4d5ac5c865163fe6825aa9b
brunosouza1977 <bmelodesouza@gmail.com>
2020-10-08 09:52:27 -0300

    Projeto iniciado

---
+++ TratamentoExcecao/Entities/Reservation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TratamentoExcecao.Entities.Exceptions;
+
+namespace TratamentoExcecao.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            //realizando tratamento no construtor para que não seja criada uma reserva
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Checkout date must be after checkin");
+            }
+
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            //função TimeSpan calcula o intervalo de tempo entre datas
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if(checkin < now || checkout < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if(checkout <= checkin)
+            {
+                throw new DomainException("Checkout date must be after checkin");
+            }
+
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room"
+                + RoomNumber
+                + ", check-in: "
+                + Checkin.ToString("dd/MM/yyyy")
+                + ", check out: "
+                + Checkout.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+
+
+
+        }
+
+
+    }
+}
---
+++ TratamentoExcecao/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using TratamentoExcecao.Entities;
+using TratamentoExcecao.Entities.Exceptions;
+
+namespace TratamentoExcecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            Console.WriteLine("Room number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check in date: (dd/mm/yyyy ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check out date: (dd/mm/yyyy ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            
+            Reservation reservation = new Reservation(number, checkin, checkout);
+            Console.WriteLine("Reservation: " + reservation);
+
+                //atualização da reserva:
+
+            Console.WriteLine();
+            Console.WriteLine("Entre com a data para atualizar a rserva: ");
+            checkin = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check out date: (dd/mm/yyyy ");
+            checkout = DateTime.Parse(Console.ReadLine());
+
+                               
+            reservation.UpdateDates(checkin, checkout);
+            Console.WriteLine("Reservation: " + reservation);
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+
+
+                }
+
+            }
+            
+            
+            
+
+        }
+    
+
